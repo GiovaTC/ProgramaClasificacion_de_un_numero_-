88c1445f3b63d9a4b686760db3293d180c348d68
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-22 09:11:38 -0500

    commit - ConexionBD .

--- src/main/java/com/ejemplo/config/ConexionBD.java
+++ src/main/java/com/ejemplo/config/ConexionBD.java
@@ -1,4 +1,22 @@
 package com.ejemplo.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+
+    private static final String URL =
+            "jdbc:oracle:thin:@localhost:1521/orcl";
+
+    private static final String USUARIO = "system";
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(
+                URL,
+                USUARIO,
+                PASSWORD
+        );
+    }   
 }
